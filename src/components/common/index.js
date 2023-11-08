6abca7a370c523eaacde0e06f3c23dae0bfe7ffb
junayed-code <junayed.code@gmail.com>
2023-11-08 23:31:01 +0600

    Add reusable commponents

---
+++ src/components/common/index.js
@@ -0,0 +1,8 @@
+export { default as Card } from "./Card";
+export { default as Field } from "./Field";
+export { default as Button } from "./Button";
+export { default as Loading } from "./Loading";
+export { default as Section } from "./Section";
+export { default as Container } from "./Container";
+export { default as StarRating } from "./StarRating";
+export { default as FieldPassword } from "./FieldPassword";

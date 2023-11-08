2823cfc1e989839d17547331f5843c8354c04066
junayed-code <junayed.code@gmail.com>
2023-11-08 23:39:52 +0600

    Add providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { AuthProvider, ThemeProvider } from "./providers";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./routes";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );

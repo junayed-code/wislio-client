007d820c61129a9f2cf8db100bb92a10c5a77f92
junayed-code <junayed.code@gmail.com>
2023-11-08 23:33:24 +0600

    Update components import

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from "./common";
+export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
+export { default as Banner } from "./Banner";

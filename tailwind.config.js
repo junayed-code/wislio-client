0521a00803bb05aa14d5b547352a833bbf1066cd
junayed-code <junayed.code@gmail.com>
2023-11-08 23:40:55 +0600

    Update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,16 @@ import daisyui from "daisyui";
 import themes from "daisyui/src/theming/themes";
 
 const lightTheme = themes["[data-theme=light]"];
-const darkTheme = themes["[data-theme=dark]"];
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["EB Garamond", "serif"],
+      },
+    },
   },
   plugins: [daisyui],
 
@@ -23,7 +26,6 @@ export default {
           "secondary-content": "#4A4015",
           neutral: "#433A3F",
           "neutral-content": "#E1DEC1",
-          "neutral-content": "#FCFCFC",
           "base-100": "#FCFCFC",
           "base-content": "#0B0500",
           "--rounded-btn": "0.375rem",
@@ -33,14 +35,13 @@ export default {
       },
       {
         dark: {
-          ...darkTheme,
+          ...lightTheme,
           primary: "#1FEA5C",
           "primary-content": "#084418",
           secondary: "#F4D35E",
           "secondary-content": "#4A4015",
           neutral: "#433A3F",
           "neutral-content": "#E1DEC1",
-          "neutral-content": "#FCFCFC",
           "base-100": "#FCFCFC",
           "base-content": "#0B0500",
           "--rounded-btn": "0.375rem",

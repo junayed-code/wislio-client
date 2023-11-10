8bf1a694db51d43de62a2959b465904694e63308
junayed-code <junayed.code@gmail.com>
2023-11-10 11:25:01 +0600

    Update nav items display logic

--- src/components/Navbar/NavItems.jsx
+++ src/components/Navbar/NavItems.jsx
@@ -5,43 +5,44 @@ const navItems = [
   {
     path: "/",
     name: "Home",
-    private: false,
   },
   {
     path: "/books",
     name: "All Books",
-    private: false,
   },
   {
     path: "/add-book",
     name: "Add Book",
-    private: true,
+    roles: ["librarian"],
   },
   {
     path: "/borrowedbooks",
     name: "Borrowed Books",
-    private: true,
+    roles: ["user", "librarian"],
   },
 ];
 
 export default function NavItems({ className = "", onItemClick }) {
   const { currentUser } = useAuth();
+  const userRole = currentUser?.role;
+
+  const displayNavItems = navItems.filter(
+    item => !item.roles || item.roles?.includes(userRole)
+  );
 
   return (
     <ul className={"text-lg ".concat(className).trim()}>
-      {navItems
-        .filter(item => !item.private || (currentUser && item.private))
-        .map(item => (
-          <li key={item.name}>
-            <NavLink
-              onClick={onItemClick}
-              className="text-base-content/75 hover:underline underline-offset-4"
-              to={item.path}
-            >
-              {item.name}
-            </NavLink>
-          </li>
-        ))}
+      {displayNavItems.map(item => (
+        <li key={item.name}>
+          <NavLink
+            onClick={onItemClick}
+            className="text-base-content/75 hover:underline underline-offset-4"
+            to={item.path}
+          >
+            {item.name}
+          </NavLink>
+        </li>
+      ))}
     </ul>
   );
 }
